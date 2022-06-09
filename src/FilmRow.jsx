1cb91f1a28cbf2b08531582848cba092a8b70f97
jmch-git <juneholmberg@gmail.com>
2022-06-09 15:52:41 -0700

    Posters added

--- src/FilmRow.jsx
+++ src/FilmRow.jsx
@@ -5,7 +5,7 @@ export default class FilmRow extends Component {
     render() {
         return (
             <div class="film-row">
-                <img src={this.props.film.poster_path} alt=""/>
+                <img src={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`} alt=""/>
                 <div className="film-summary">
                     <h1>{this.props.film.title}</h1>
                     <p>{this.props.film.release_date}</p>
